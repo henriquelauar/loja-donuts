dd5ac983e6a0d8116b3c3b44a8f87e4acf339a8e
henrique lauar <henriquelauar2002@gmail.com>
2022-03-28 21:57:08 -0300

    Adding import of the components to the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import Header from './Components/Header';
+import Banner from './Components/Banner';
+import Main from './Components/Main'
+import CarouselComponent from './Components/Carousel';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Banner />
+      <Main />
+      <CarouselComponent />
     </div>
   );
 }
