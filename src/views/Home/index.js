15fc5d728acc1c55fa271fa0a0c0853efdefda80
henrique lauar <henriquelauar2002@gmail.com>
2022-04-04 23:21:24 -0300

    Adding style to the home page

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -3,10 +3,11 @@ import Header from '../../Components/Header';
 import Banner from '../../Components/Banner';
 import Main from '../../Components/Main'
 import CarouselComponent from '../../Components/Carousel';
+import styles from './styles.module.css'
 
 export const Home = () => {
     return(
-        <div className="App">
+        <div className={styles.App} >
             <Header />
             <Banner />
             <Main />
