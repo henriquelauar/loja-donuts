cd8ec2247839b9e752b2f31d7d27982b2c46510e
henrique lauar <henriquelauar2002@gmail.com>
2022-04-04 23:23:16 -0300

    Adding header to Franqueado and Galeria page

--- src/views/Franqueado/index.js
+++ src/views/Franqueado/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from '../../Components/Header';
 
-export const Galeria = () => {
+export const Franquiado = () => {
     return (
         <Header />
     )
