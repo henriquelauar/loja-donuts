898ce1f523c96a3a94ef6f8cea2c6057726184ec
henrique lauar <henriquelauar2002@gmail.com>
2022-04-04 23:27:59 -0300

    Deleting comment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
-import Header from './Components/Header';
-import Banner from './Components/Banner';
-import Main from './Components/Main'
-import CarouselComponent from './Components/Carousel';
 import Rotas from './routes';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />
-      <Banner />
-      <Main />
-      <CarouselComponent /> */}
       <Rotas>
-
+        
       </Rotas>
     </div>
   );
