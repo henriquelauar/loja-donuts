d31ee826fef86d5e507968fc59125490b267fdfa
henrique lauar <henriquelauar2002@gmail.com>
2022-04-06 01:06:26 -0300

    Adjusting components to the new background'

--- src/Components/BannerGaleria/index.js
+++ src/Components/BannerGaleria/index.js
@@ -5,7 +5,7 @@ const BannerGaleria = () => {
     <>
       <div>
         <h1 className={styles.title}>
-          Conheça nossos <span className={styles.title2}>Sabores e Combos</span>
+          Conheça nossos <span style={{ color: "#0E2E3B" }}>Sabores e Combos</span>
         </h1>
       </div>
     </>
--- src/Components/BannerHome/index.js
+++ src/Components/BannerHome/index.js
@@ -1,28 +1,33 @@
 import styles from "./styles.module.css";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-import { Box } from "@mui/material";
 
 const Banner = () => {
   return (
-    <>
-      <Box className={styles.bannerImage}>
-        
-      </Box>
-      <Box className={styles.bannerTitle}>
-        <h1 className={styles.bannerTitle1}>A hora mais gostosa do seu dia <span style={{color: '#0E2E3B'}}>é com a gente </span>
+    <div className={styles.container}>
+      <div className={styles.bannerTitle}>
+        <h1 className={styles.bannerTitle1}>
+          A hora mais gostosa do seu dia{" "}
+          <span style={{ color: "#0E2E3B" }}>é com a gente </span>
         </h1>
-      </Box>
-      <p className={styles.bannerText}>
-        Os Donuts mais deliciosos, fresquinhos e LINDOS! <br/>Sua pausa nunca mais
-        será a mesma com a explosão de sabores e cores que produzimos
-        diariamente pra você!
-      </p>
-      <a className={styles.a} href="https://api.whatsapp.com/send?phone=5533988051975&">
-        <button className={styles.btnPedido}>
-          Faça seu pedido <WhatsAppIcon className={styles.icon} />
-        </button>
-      </a>
-    </>
+      </div>
+      <div className={styles.bannerText}>
+        <p>
+          Os Donuts mais deliciosos, fresquinhos e LINDOS! <br />
+          Seus dias nunca mais serão os mesmos após experimentar a explosão de
+          sabores e cores que produzimos diariamente pra você!
+        </p>
+      </div>
+      <div>
+        <a
+          className={styles.a}
+          href="https://api.whatsapp.com/send?phone=5533988051975&"
+        >
+          <button className={styles.btnPedido}>
+            Faça seu pedido <WhatsAppIcon className={styles.icon} />
+          </button>
+        </a>
+      </div>
+    </div>
   );
 };
 
