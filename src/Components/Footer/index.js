bf75e57f86d818f71c128ab58bb2cbf483cc802e
henrique lauar <henriquelauar2002@gmail.com>
2022-04-07 16:15:22 -0300

    Adding empty div to align footer to header

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -6,16 +6,36 @@ import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 const Footer = () => {
   return (
     <div className={styles.container}>
+      <div></div>
       <div>
         <h4>Redes Sociais</h4>
-        <FacebookIcon style={{ fontSize: "42px", color: "#ED708E" }} />
-        <InstagramIcon style={{ fontSize: "42px", color: "#ED708E" }} />
+        <div className={styles.redesIcones}>
+          <div className={styles.iconesDiv}>
+            <FacebookIcon
+              className={styles.icones}
+              style={{ fontSize: "26px", color: "#ffffff" }}
+            />
+          </div>
+          <div className={styles.iconesDiv}>
+            <InstagramIcon
+              className={styles.icones}
+              style={{ fontSize: "26px", color: "#ffffff" }}
+            />
+          </div>
+        </div>
       </div>
+      <div></div>
+      <div></div>
       <img src="/assets/footer.png" alt="" />
+      <div></div>
+      <div></div>
       <div>
         <h4>Contato</h4>
-        <WhatsAppIcon style={{ fontSize: "42px", color: "#ED708E" }} />
+        <div className={styles.whatsapp}>
+          <WhatsAppIcon style={{ fontSize: "26px", color: "#ffffff" }} />
+        </div>
       </div>
+      <div></div>
     </div>
   );
 };
